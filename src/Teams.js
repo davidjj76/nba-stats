fd514112c4dc97f97aa7f4cdde80dfc39bd193c2
David Jiménez <davidjj76@gmail.com>
2019-10-08 01:31:11 +0200

    Custom hook useFetch

--- src/Teams.js
+++ src/Teams.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Fetch from './Fetch';
+import useFetch from './useFetch';
 import Loading from './Loading';
 
 const renderTeam = ({ id, full_name }) => <li key={id}>{full_name}</li>;
 
 function Teams() {
+  const teams = useFetch('https://www.balldontlie.io/api/v1/teams');
   return (
-    <Fetch url="https://www.balldontlie.io/api/v1/teams">
-      {teams => (
-        <Loading isLoading={() => !teams} loadingText="Loading teams...">
-          {() => (
-            <div>
-              <Link to="/">Back</Link>
-              <ul>{teams.map(renderTeam)}</ul>
-            </div>
-          )}
-        </Loading>
+    <Loading isLoading={() => !teams} loadingText="Loading teams...">
+      {() => (
+        <div>
+          <Link to="/">Back</Link>
+          <ul>{teams.map(renderTeam)}</ul>
+        </div>
       )}
-    </Fetch>
+    </Loading>
   );
 }
 
